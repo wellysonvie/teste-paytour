9d11917a06fac1a6b920fbaa1d3b6058616871e6
wellysonvie <wellysonvie@gmail.com>
2021-03-24 18:42:58 -0300

    refactor: validate add resume

--- source/Models/Resume.php
+++ source/Models/Resume.php
@@ -13,36 +13,30 @@ class Resume extends DataLayer
 
     public function add($name, $email, $phone, $role, $education, $observation, $file): Resume
     {
-        $hasError = false;
-
         if (!$email) {
             alert('E-mail inválido!', 'danger');
-            $hasError = true;
+            return null;
         }
 
         if (!$education || $education <= 0 || $education > 7) {
             alert('Nível de escolaridade inválido!', 'danger');
-            $hasError = true;
+            return null;
         }
 
         if (empty($name) || empty($phone) || empty($role)) {
             alert('Os campos: nome, telefone e cargo devem ser preenchidos!', 'danger');
-            $hasError = true;
+            return null;
         }
 
-        if ($hasError) {
-            return null;
-        }else {
-            $this->name = trim($name);
-            $this->email = trim($email);
-            $this->phone = trim($phone);
-            $this->role = trim($role);
-            $this->education = trim($education);
-            $this->observation = trim($observation);
-            $this->file = $file;
-            $this->ip = $_SERVER["REMOTE_ADDR"];
-            $this->save();
-        }
+        $this->name = trim($name);
+        $this->email = trim($email);
+        $this->phone = trim($phone);
+        $this->role = trim($role);
+        $this->education = trim($education);
+        $this->observation = trim($observation);
+        $this->file = $file;
+        $this->ip = $_SERVER["REMOTE_ADDR"];
+        $this->save();
 
         return $this;
     }
